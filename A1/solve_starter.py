d4c128aeeb8f00131f1d183f94cf1547429f8b80
padirmal <markus@markus.com>
2023-10-09 13:48:06 -0400

    Update files

--- A1/solve_starter.py
+++ A1/solve_starter.py
@@ -26,19 +26,8 @@ def is_goal(state):
     :rtype: bool
     """
 
-    #Approach 2
-    box_set = set(state.board.boxes)
-    storage_set = set(state.board.storage)
-    if box_set==storage_set:
-        return True
-    return False
+    raise NotImplementedError
 
-    #raise NotImplementedError
-
-
-
-#########################################################################
-#Path Functions
 
 def get_path(state):
     """
@@ -50,23 +39,8 @@ def get_path(state):
     :return: The path.
     :rtype: List[State]
     """
-    return recursive_trace(state, [])
 
-
-
-def recursive_trace(state, path: list):
-    if state.parent==None:
-        path.append(state)
-        return path
-    
-    else:
-        path = recursive_trace(state.parent, path)
-        path.append(state)
-        return path
-
-
-#########################################################################
-#Move Functions
+    raise NotImplementedError
 
 
 def get_successors(state):
@@ -80,116 +54,7 @@ def get_successors(state):
     :rtype: List[State]
     """
 
-    board = state.board
-    successor_states=[]
-
-    for robot in board.robots:
-        moves = identify_moves(robot, state)
-        for move in moves:
-            successor_states.append(fetch_successor(robot, move, state))
-
-    return successor_states
-
-
-def neighbourhood(position: tuple[int, int], board):
-    """
-    Returns a list containing the neighbourhood of the
-    entity at the given position
-
-    Args:
-        position (tuple[int, int]): Position of the entity
-        board (_type_): Current board
-
-    Returns:
-        list[tuple[int, int]]: List of the neighbouring coordinates 
-        for an entity
-    """
-    neighbourhood = set()
-    neighbourhood.add((position[0]+1, position[1]))
-    neighbourhood.add((position[0]-1, position[1]))
-    neighbourhood.add((position[0], position[1]+1))
-    neighbourhood.add((position[0], position[1]-1))
-        
-    return neighbourhood
-
-
-def identify_moves(robot: tuple[int, int], state):
-    """
-    Returns a list of available moves for the given robot 
-    by analyzing its neighbourhood.
-
-    Args:
-        robot (tuple[int]): Position of the robot
-        state (_type_): Current state
-
-    Returns:
-        list[tuple[int, int]]: List of the possible moves 
-    """
-    robot_nbd = neighbourhood(robot, state.board)
-
-    #accounting for obstacles in robot's neighbourhood
-    robot_nbd -= set(state.board.obstacles)
-
-
-    #robot->box->wall: movement not possible
-    robot_adjacent = robot_nbd.intersection(set(state.board.boxes))
-    for box in robot_adjacent:
-        #box_nbd: set containing obstacles adjacent to box
-        box_nbd=neighbourhood(box, state.board).intersection(
-            set(state.board.obstacles))
-
-        #if robot->box->wall, then direction(r->b)=direction(b->w)
-        direction=(robot[0]-box[0], robot[1]-box[1])
-
-        invalid_check=[(box[0]-obs[0], box[1]-obs[1])==direction 
-                          for obs in box_nbd]
-        
-        if True in invalid_check:
-            robot_nbd -= set([box])
-
-    return list(robot_nbd)
-
-
-def fetch_successor(robot: tuple[int, int], move: tuple[int, int], state):
-    """
-    Returns the successor for the provided state, given the position 
-    of the robot and the next move.
-
-    Args:
-        robot (tuple[int, int]): Position of robot
-        move (tuple[int, int]): Position resulting from move
-        state (State): Current state
-
-    Returns:
-        State: The state resulting from move
-    """
-    new_board = Board(state.board.name, state.board.width, state.board.height,
-                      state.board.robots.copy(), state.board.boxes.copy(),
-                      state.board.storage.copy(), state.board.obstacles.copy())
-    successor = State(new_board, state.hfn, state.f-1, state.depth+1, state)
-    successor.board.boxes = set(successor.board.boxes)    
-    successor.board.robots = set(successor.board.robots)
-    successor.board.storage = set(successor.board.storage)
-    successor.board.obstacles = set(successor.board.obstacles)
-
-    #move results in pushing a box
-    if move in successor.board.boxes:
-        direction = (robot[0]-move[0], robot[1]-move[1])
-        box_update = (move[0]-direction[0], move[1]-direction[1])
-
-        successor.board.boxes.remove(move) #move and box refer to the same position
-        successor.board.boxes.add(box_update)
-
-    successor.board.robots.remove(robot)
-    successor.board.robots.add(move)
-
-    return successor
-
-
-
-#########################################################################
-# Search Algorithms 
-    
+    raise NotImplementedError
 
 
 def dfs(init_board):
@@ -206,31 +71,8 @@ def dfs(init_board):
     :return: (the path to goal state, solution cost)
     :rtype: List[State], int
     """
-    init_state = State(board=init_board, hfn=None, f=0, depth=0,
-                  parent=None)
-    frontier = [init_state]
-    explored=set()
-    
-    while not len(frontier)==0:
-        state = frontier.pop()
-        print(state.board)
-        
 
-        if state.board in explored: 
-            continue
-
-        elif state.board not in explored:
-            explored.add(state.board)
-
-            if is_goal(state):
-                return get_path(state), state.depth
-        
-            else:
-                frontier.extend(get_successors(state))
-        
-
-    return [], -1
-    
+    raise NotImplementedError
 
 
 def a_star(init_board, hfn):
@@ -249,31 +91,8 @@ def a_star(init_board, hfn):
     :return: (the path to goal state, solution cost)
     :rtype: List[State], int
     """
-    f_val = hfn(init_board)
-    init_state = State(board=init_board, hfn=hfn, f=f_val, depth=0,
-                  parent=None)
-    frontier = [init_state]
-    explored=set()
-    
-    while not len(frontier)==0:
-        state = heappop(frontier)
-        print(state.board)
-        
 
-        if state.board in explored: 
-            continue
-
-        elif state.board not in explored:
-            explored.add(state.board)
-
-            if is_goal(state):
-                return get_path(state), state.depth
-        
-            else:
-                successors = get_successors(state)
-                for successor in successors:
-                    heappush(frontier, successor)
-    return [], -1
+    raise NotImplementedError
 
 
 def heuristic_basic(board):
@@ -289,22 +108,8 @@ def heuristic_basic(board):
     :return: The heuristic value.
     :rtype: int
     """
-    boxes = board.boxes.copy()
-    available_storage = board.storage.copy()
-    #dictionary mapping a box to a storage point
-    heuristic_val = 0
 
-    for box in boxes:
-        distances = [abs(box[0]-store[0])+abs(box[1]-store[1])
-                    for store in available_storage]
-        
-        min_dist = min(distances)
-        heuristic_val += min_dist
-        min_index = distances.index(min_dist)
-        available_storage.pop(min_index)
-        boxes.pop(min_index)
-
-    return heuristic_val
+    raise NotImplementedError
 
 
 def heuristic_advanced(board):
@@ -317,70 +122,8 @@ def heuristic_advanced(board):
     :rtype: int
     """
 
-    """
-    Returns heuristic value for the given board based on the number of deadlocks.
-    A deadlock is a position which makes the board unsolvable.
-    Different types of deadlocks are penalized differently.
+    raise NotImplementedError
 
-    Args:
-        board (_type_): Current board
-    """
-    penalty={'corner':math.inf, 'wall_but_storage':1, 'wall_no_storage':math.inf,
-             'box_wall':2}
-    heuristic = 0
-    boxes = board.boxes
-
-    corners = [is_corner(box, board) for box in boxes]
-    heuristic = heuristic_basic(board)
-    if any(corners):
-        heuristic+= math.inf
-
-    return heuristic
-
-
-
-def is_corner(box: tuple[int, int], board):
-    """
-    Returns True if the box is located
-        1. at a corner
-        2. along a wall with no available storage
-           (eventually leading to a corner)
-    A corner may consist of only obstacles, or both boxes and obstacles
-
-    Args:
-        box (tuple[int, int]): Position of box
-    """
-    box_nbd = neighbourhood(box, board)
-    storage = board.storage
-    surrounding =  box_nbd.intersection(set(board.obstacles))
-    surrounding = surrounding.intersection(set(board.boxes))
-
-    #at corner
-    if len(surrounding) > 2 and box not in storage:
-        return True
-    #at corner
-    if len(surrounding) ==2 and box not in storage:
-        wall0 = surrounding[0]
-        wall1 = surrounding[1]
-        direction0 = (wall0[0]-box[0], wall0[1]-box[1])
-        direction1 = (box[0]-wall1[0], box[1]-wall1[1])
-        if direction0!=direction1:
-            return True
-        
-
-    #along a wall, leading to a corner
-    if len(surrounding)==1 and surrounding[0] in board.obstacles and box not in storage:
-        direction = (box[0]-surrounding [0], box[1]-surrounding [1])
-        if direction in [(1, 0), (-1, 0)]:
-            check_storage =[True for store in board.storage if box[0]==store[0]]
-            if not any(check_storage):
-                return True
-        elif direction in [(0, 1), (0,-1)]:
-            check_storage = [True for store in board.storage if box[1]==store[1]]
-            if not any(check_storage):
-                return True
-
-    return False
 
 def solve_puzzle(board: Board, algorithm: str, hfn):
     """
